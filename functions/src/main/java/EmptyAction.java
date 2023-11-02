17fbbdbb4d94220bcc5e3e581a1bc1a0c8cf91ed
Robert-Martins <robertcantares@gmail.com>
2023-11-02 11:32:06 -0300

    feat/code-implemented-documentation

--- functions/src/main/java/EmptyAction.java
+++ functions/src/main/java/EmptyAction.java
@@ -1,6 +1,21 @@
+
+/**
+*
+* Represents a Function without arguments and no return                         <br><br>
+*
+* Author: Robert Martins                                                        <br>
+*
+* Github: github.com/Robert-Martins
+*
+*/
 @FunctionalInterface
 public interface EmptyAction {
 
+    /**
+    *
+    * Executes the EmptyAction
+    *
+    */
     void run();
 
 }
